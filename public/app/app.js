75cee6a0bf23bf26f7afd50b2af2256ab8eba36d
vcarreno96 <heatfan24@gmail.com>
2015-06-28 14:25:55 -0400

    all we need to get started

---
+++ public/app/app.js
@@ -0,0 +1,8 @@
+// declare global dependency list for app module only once (usually in config.js)
+angular.module('starterApp', []);
+
+// add a controller to the app module
+angular.module('starterApp')
+.controller('testController', function($scope) {
+	$scope.test = 'Welcome to Angular!';
+})
