cefccd191f825ccbd124dc200c9e6f9518cd8e8b
victor.carreno <vcarreno@dssinc.com>
2015-06-29 15:54:07 -0400

    added example

--- examples/globals/app.js
+++ examples/globals/app.js
@@ -10,3 +10,20 @@ angular.module('starterApp')
 function testController($scope){
 	console.log('this is everywhere %s', $scope);
 }
+
+
+// 
+// (function() { // wrapping your own code in an IIFE allows use of function form of 'use strict'
+// 'use strict';
+// 
+// // declare global dependency list for app module only once (usually in config.js)
+// angular.module('starterApp', []);
+// 
+// // add controllers to the app module
+// angular.module('starterApp')
+// .controller('testController', function($scope) {
+// 	$scope.test = 'Welcome to Angular!';
+// });	
+// 	
+// 	
+// }());
